e69b8b41d1a77b8dd7796d38a5d8ac1ff374e117
Liu Yuancheng <liu_yuan_cheng@hotmail.com>
2020-08-21 13:47:51 +0800

    Add PLC flag for UI init to select whether display PLC control part.

--- src/serialCtrl.py
+++ src/serialCtrl.py
@@ -12,15 +12,18 @@
 #-----------------------------------------------------------------------------
 import os, sys
 import time
-import serial
 import glob
+import serial
 import wx
-import M2PLC221 as m221
-import S7PLC1200 as s71200
+
+PLC_FLAG = False # flag whether we want to control PLC from the serialCtrl program. 
+
+if PLC_FLAG:
+    import M2PLC221 as m221
+    import S7PLC1200 as s71200
 
 PERIODIC = 500      # update in every 500ms
-
-
+WIN_H = 400 if PLC_FLAG else 320    # window height.
 
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
@@ -28,14 +31,14 @@ class UIFrame(wx.Frame):
     """ URL/IP gps position finder main UI frame."""
     def __init__(self, parent, id, title):
         """ Init the UI and parameters """
-        wx.Frame.__init__(self, parent, id, title, size=(250, 320))
+        wx.Frame.__init__(self, parent, id, title, size=(300, WIN_H))
         self.SetBackgroundColour(wx.Colour(200, 210, 200))
         # Set the periodic call back
         self.lastPeriodicTime = time.time()
         self.timer = wx.Timer(self)
         self.updateLock = False
         self.dataList = ['0', '0', 'off', 'off', 'red', 'red', 'off', 'on']
-        self.fieldlList = []
+        self.fieldlList = [] # data control field list.
         # init the serial communication:
         portList = []
         if sys.platform.startswith('win'):
@@ -56,18 +59,24 @@ class UIFrame(wx.Frame):
             except (OSError, serial.SerialException):
                 pass
         print(('COM connection: the serial port can be used :%s' % str(portList)))
-
-        self.serialPort = portList[0]
-
-        try:
-            self.serComm = serial.Serial(self.serialPort, 115200, 8, 'N', 1, timeout=1)
-        except:
-            print("Serial connection: serial port open error.")
-            #return None
-        # connect to the PLC
-        self.se1 = m221.M221('192.168.10.72')
-        self.se2 = s71200.S7PLC1200('192.168.10.73')
-        self.se3 = m221.M221('192.168.10.71')
+        # Try to setup the serial connection.
+        if len(portList)> 0:
+            self.serialPort = portList[0] 
+            try:
+                baudRate = 115200
+                self.serComm = serial.Serial(self.serialPort, baudRate, 8, 'N', 1, timeout=1)
+            except:
+                print("Error: Serial connection: serial port open error.")
+                #return None
+        else:
+            self.serComm = None
+            print("Warning: No connection can be used!")
+        # Try to setup the PLC connection.
+        if PLC_FLAG:
+            # connect to the PLC
+            self.se1 = m221.M221('192.168.10.72')
+            self.se2 = s71200.S7PLC1200('192.168.10.73')
+            self.se3 = m221.M221('192.168.10.71')
 
         self.SetSizer(self._buidUISizer())
         self.statusbar = self.CreateStatusBar()
@@ -127,29 +136,30 @@ class UIFrame(wx.Frame):
         self.fieldlList.append(sirenCB)
         gs.Add(sirenCB, flag=flagsR, border=2)
 
+        if PLC_FLAG:
+            gs.Add(wx.StaticText(self, label=' Pump speed : '), flag=flagsR, border=2)
+            self.pumpSP= wx.ComboBox(self, -1, choices=['off', 'low', 'high'])
+            self.pumpSP.SetSelection(0)
+            gs.Add(self.pumpSP, flag=flagsR, border=2)
 
-        gs.Add(wx.StaticText(self, label=' Pump speed : '), flag=flagsR, border=2)
-        self.pumpSP= wx.ComboBox(self, -1, choices=['off', 'low', 'high'])
-        self.pumpSP.SetSelection(0)
-        gs.Add(self.pumpSP, flag=flagsR, border=2)
 
+            gs.Add(wx.StaticText(self, label=' Moto speed : '), flag=flagsR, border=2)
+            self.MotoSP= wx.ComboBox(self, -1, choices=['off', 'low', 'high'])
+            self.MotoSP.SetSelection(0)
+            gs.Add(self.MotoSP, flag=flagsR, border=2)
 
-        gs.Add(wx.StaticText(self, label=' Moto speed : '), flag=flagsR, border=2)
-        self.MotoSP= wx.ComboBox(self, -1, choices=['off', 'low', 'high'])
-        self.MotoSP.SetSelection(0)
-        gs.Add(self.MotoSP, flag=flagsR, border=2)
+            gs.Add(wx.StaticText(self, label=' All sensor control : '), flag=flagsR, border=2)
+            self.senPower= wx.ComboBox(self, -1, choices=['on', 'off'])
+            self.senPower.SetSelection(0)
+            gs.Add(self.senPower, flag=flagsR, border=2)
 
-        gs.Add(wx.StaticText(self, label=' All sensor control : '), flag=flagsR, border=2)
-        self.senPower= wx.ComboBox(self, -1, choices=['on', 'off'])
-        self.senPower.SetSelection(0)
-        gs.Add(self.senPower, flag=flagsR, border=2)
-
-        gs.Add(wx.StaticText(self, label=' All power control : '), flag=flagsR, border=2)
-        self.AllPower= wx.ComboBox(self, -1, choices=['on', 'off'])
-        self.AllPower.SetSelection(0)
-        gs.Add(self.AllPower, flag=flagsR, border=2)
+            gs.Add(wx.StaticText(self, label=' All power control : '), flag=flagsR, border=2)
+            self.AllPower= wx.ComboBox(self, -1, choices=['on', 'off'])
+            self.AllPower.SetSelection(0)
+            gs.Add(self.AllPower, flag=flagsR, border=2)
+        
+        # Setting enalbe buton.
         gs.AddSpacer(5)
-
         self.sendBt = wx.Button(self, label='Set')
         self.sendBt.Bind(wx.EVT_BUTTON, self.onSend)
         gs.Add(self.sendBt, flag=flagsR, border=2)
@@ -165,6 +175,7 @@ class UIFrame(wx.Frame):
             print("main frame update at %s" % str(now))
             self.lastPeriodicTime = now
 
+#--UIFrame---------------------------------------------------------------------
     def onSend(self, event):
         msgStr = ':'.join([item.GetValue() for item in self.fieldlList])#+'\r'
         self.statusbar.SetStatusText(msgStr)
@@ -172,43 +183,47 @@ class UIFrame(wx.Frame):
             print('Send message [%s] to cmd ' %msgStr)
             self.serComm.write(msgStr.encode('utf-8'))
         time.sleep(0.1)
-        if self.pumpSP.GetSelection() == 0:
-            self.se1.writeMem('M4', 0)
-            self.se1.writeMem('M5', 0)
-        elif self.pumpSP.GetSelection() == 1:
-            self.se1.writeMem('M4', 0)
-            self.se1.writeMem('M5', 1)
-        elif self.pumpSP.GetSelection() == 2:
-            self.se1.writeMem('M4', 1)
-            self.se1.writeMem('M5', 0)
-        time.sleep(0.1)
-        if self.MotoSP.GetSelection() == 0:
-            self.se2.writeMem('qx0.3', False)
-            self.se2.writeMem('qx0.4', False)
-        elif self.MotoSP.GetSelection() == 1:
-            self.se2.writeMem('qx0.3', False)
-            self.se2.writeMem('qx0.4', True)
-        elif self.MotoSP.GetSelection() == 2:
-            self.se2.writeMem('qx0.3', True)
-            self.se2.writeMem('qx0.4', False)
-        time.sleep(0.1)
-        if self.senPower.GetSelection() == 0:
-            self.se3.writeMem('M4', 1)
-            self.se3.writeMem('M5', 1)
-        elif self.senPower.GetSelection() == 1:
-            self.se3.writeMem('M4', 0)
-            self.se3.writeMem('M5', 0)
-        time.sleep(0.1)
-        if self.AllPower.GetSelection() == 0:
-            self.se3.writeMem('M6', 0)
-        elif self.AllPower.GetSelection() == 1:
-            self.se3.writeMem('M6', 1)
+        if PLC_FLAG:
+            if self.pumpSP.GetSelection() == 0:
+                self.se1.writeMem('M4', 0)
+                self.se1.writeMem('M5', 0)
+            elif self.pumpSP.GetSelection() == 1:
+                self.se1.writeMem('M4', 0)
+                self.se1.writeMem('M5', 1)
+            elif self.pumpSP.GetSelection() == 2:
+                self.se1.writeMem('M4', 1)
+                self.se1.writeMem('M5', 0)
+            time.sleep(0.1)
+
+            if self.MotoSP.GetSelection() == 0:
+                self.se2.writeMem('qx0.3', False)
+                self.se2.writeMem('qx0.4', False)
+            elif self.MotoSP.GetSelection() == 1:
+                self.se2.writeMem('qx0.3', False)
+                self.se2.writeMem('qx0.4', True)
+            elif self.MotoSP.GetSelection() == 2:
+                self.se2.writeMem('qx0.3', True)
+                self.se2.writeMem('qx0.4', False)
+            time.sleep(0.1)
+
+            if self.senPower.GetSelection() == 0:
+                self.se3.writeMem('M4', 1)
+                self.se3.writeMem('M5', 1)
+            elif self.senPower.GetSelection() == 1:
+                self.se3.writeMem('M4', 0)
+                self.se3.writeMem('M5', 0)
+            time.sleep(0.1)
+
+            if self.AllPower.GetSelection() == 0:
+                self.se3.writeMem('M6', 0)
+            elif self.AllPower.GetSelection() == 1:
+                self.se3.writeMem('M6', 1)
 
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
 class MyApp(wx.App):
     def OnInit(self):
-        iMainFrame = UIFrame(None, -1, "Serial Comm tester")
+        iMainFrame = UIFrame(None, -1, "Serial Comm Tester")
         iMainFrame.Show(True)
         return True
 
